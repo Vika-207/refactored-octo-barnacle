496ce1d2924ec593a761d3153df50d43852be96b
Vika-207 <111182393+Vika-207@users.noreply.github.com>
2022-08-26 14:52:30 +0500

    apps.py
    from django.apps import AppConfig
    
    
    class HelloworldappConfig(AppConfig)
    name = 'HelloworldApp'
    from django.apps import AppConfig
    
    
    class EncyclopediaConfig(AppConfig):
        name = 'encyclopedia'

---
+++ apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class HelloworldappConfig(AppConfig)
+name = 'HelloworldApp'
+from django.apps import AppConfig
+
+
+class EncyclopediaConfig(AppConfig):
+    name = 'encyclopedia'
