b9300af714d85823f37853053289d8f3afed470c
Vika-207 <111182393+Vika-207@users.noreply.github.com>
2022-08-26 15:23:09 +0500

    Add files via upload

--- apps.py
+++ apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
 
 
-class HelloworldappConfig(AppConfig)
-name = 'HelloworldApp'
-from django.apps import AppConfig
-
-
 class EncyclopediaConfig(AppConfig):
     name = 'encyclopedia'
--- models.py
+++ models.py
@@ -1,23 +1,3 @@
 from django.db import models
 
-
-class Topic(models.Model):
-    top_name = models.CharField(max_lenght=264,unique=True
-                                
-    def__str__(self):
-       return self.top_name
-
- class Webpage(models.Model):
-     topic = models.ForeignKey(Topic)
-     name = models.Charfield(max_lenght=264.unique=True
-     url = URLField(unique)
-                            
-     def__str__(self):
-        return self.name
-                             
- class AccessRecord(models.Model):
-     name = models.ForeignKey(Webpage)
-     date = models.DataField()
-    
-     def__str__(self):
-        return str(self.date)
+# Create your models here.
